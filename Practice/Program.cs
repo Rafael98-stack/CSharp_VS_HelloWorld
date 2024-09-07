4bd01225b706f4d38e5ad91c1ee5762152ed8ff8
Rafael98-stack <rafaelcasapaojr@gmail.com>
2024-09-07 10:32:09 +0200

    Aggiungere i file di progetto.

---
+++ Practice/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("I'm in my own line");
+Console.Write("We are "); /* Sono un Commento
+   Multi linea "/*"                        */
+Console.Write("In the same line"); // Sono una Singola linea di Commento "//"
